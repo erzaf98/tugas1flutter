5048aea963c6108eda957ce72115a3028cae6b97
erzaf98 <86522946+erzaf98@users.noreply.github.com>
2023-10-04 02:42:46 +0800

    Add files via upload

---
+++ bin/24_manipulasiSet.dart
@@ -0,0 +1,8 @@
+void main() {
+  var names = <String>[];
+  names.add('Eko');
+  names.add('Kurniawan');
+  names.add('Khannedx');
+  print(names);
+  print(names.length);
+}
---
+++ bin/27_symbol.dart
@@ -0,0 +1,6 @@
+void main() {
+  Symbol symbol1 = Symbol('erza');
+  var symbol2 = #belajar;
+  print(symbol1);
+  print(symbol2);
+}
